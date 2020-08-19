db86013653903bca17a82d7f5aaf4fb289c2bf1e
taroj khan <tarujkhan@yahoo.com>
2020-08-19 21:15:40 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,4 +4,10 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def position_taken?(board, index)
   if board[index] == " "
     false
+  elsif board[index] == ""
+    false 
+  elsif board[index] == nil 
+    false 
+  elsif board[index] == "X" || "O"
+    true
 end
